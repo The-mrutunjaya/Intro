c4a6eb6696be6b6a29936f535e6eb76802837cf0
The-Mrutunjaya <mytabgopad@gmail.com>
2025-06-30 21:42:32 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoadingScreen from './components/LoadingScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <LoadingScreen />
+    </div>
+  );
+}
+
+export default App;
